bb1a59967156e9920a13e6ef012cd310ee0f4574
JiaqiLU0912 <jiaqilu0818@gmail.com>
2026-09-07 20:11:29 +0800

    fix typo

--- src/background.js
+++ src/background.js
@@ -133,7 +133,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 });
 
 // Toggle extension on icon click
-// Logic adapted from Kyle McDonald’s COVID Pause (https://github.com/kylemcdonald/COVIDPause)
+// Logic adapted from Kyle McDonald's COVID Pause (https://github.com/kylemcdonald/COVIDPause)
 const toggleBadgeStatus = (isEnabled) => {
   chrome.action.setBadgeBackgroundColor({ color: 'white' });
   const badgeText = isEnabled ? '%Art' : 'Off';
